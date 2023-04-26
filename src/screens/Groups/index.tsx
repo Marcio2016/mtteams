496fc35f43ca1c46a4d7760a3d6af5bbbeb01663
Marcio Ferreira <marcio.ferreira@senado.leg.br>
2023-04-26 10:09:19 -0300

    Criação do projeto e configuração do babelimports

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native"
+import { Container, Titulo } from "./styles"
+
+export const Groups = () => {
+    return(
+        <Container>
+            <Titulo>Groups!!</Titulo>
+        </Container>
+    )
+}
