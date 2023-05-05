d85dda61d7f5fa68cfb607c06ba6ed76da1a0739
Marcio Ferreira <marcio.ferreira@senado.leg.br>
2023-05-05 10:14:43 -0300

    Config fonts roboto google

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
 import { ThemeProvider } from 'styled-components'
-
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { ActivityIndicator } from 'react-native'
 import theme from '@theme/index';
 
 import { Groups } from '@screens/Groups';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+
+
   return (    
     <ThemeProvider theme={theme}>
-      <Groups />
+      { fontsLoaded ? <Groups /> : <ActivityIndicator /> }
     </ThemeProvider>
   );
 }
