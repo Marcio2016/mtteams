fdae63a155e0f0f1d2bd823eadfae69ba47690e1
Marcio Ferreira <marcio.ferreira@senado.leg.br>
2023-05-05 19:14:42 -0300

    Componente Highlight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,13 @@
-import { View, Text } from "react-native"
-import { Container, Titulo } from "./styles"
+import { Container } from "./styles"
 import { Header } from "@components/Header"
+import { Highlight } from "@components/Highlight"
+
 
 export const Groups = () => {
     return(
         <Container>
             <Header />
+            <Highlight title="Turma" subtitle="Jogue com a sua turma"/>            
         </Container>
     )
 }
