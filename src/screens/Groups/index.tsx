3cd5f9fa3b0a331d2b870dd8c534f9155f53b94e
Marcio Ferreira <marcio.ferreira@senado.leg.br>
2023-05-05 17:18:15 -0300

    Criando Componente Header

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from "react-native"
 import { Container, Titulo } from "./styles"
+import { Header } from "@components/Header"
 
 export const Groups = () => {
     return(
         <Container>
-            
+            <Header />
         </Container>
     )
 }
