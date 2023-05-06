1c5cecd2b79e1d39594558e85afde15a008795fb
Marcio Ferreira <marcio.ferreira@senado.leg.br>
2023-05-06 16:56:04 -0300

    Listando grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,14 +2,27 @@ import { GroupCard } from "@components/GroupCard"
 import { Container } from "./styles"
 import { Header } from "@components/Header"
 import { Highlight } from "@components/Highlight"
+import { useState } from "react"
+import { FlatList } from "react-native"
 
 
 export const Groups = () => {
+    
+    const [groups, setGroups] = useState<string[]>(['Galera Dev', 'Guerreiros Z'])
+    
     return(
         <Container>
             <Header />
             <Highlight title="Turma" subtitle="Jogue com a sua turma"/>
-            <GroupCard title="Galera do React"/>
+
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({item}) => (
+                    <GroupCard title={item}/>
+                )}            
+            />
+
         </Container>
     )
 }
