310e6e6baae3c65d6060a7aaf80510187643bd86
Marcio Ferreira <marcio.ferreira@senado.leg.br>
2023-05-22 18:20:48 -0300

    Tratando exceções

--- src/screens/NewGroups/index.tsx
+++ src/screens/NewGroups/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Alert } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Header } from "@components/Header";
 import { Container, Content, Icon } from "./styles";
@@ -6,15 +7,30 @@ import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { groupCreate } from "@storage/group/groupCreate";
+import { AppError } from "@utils/AppError";
 
 export function NewGroups() {
     const [group, setGroup] = useState('')
     const navigation = useNavigation()
 
-   async function handleNew() {
-        await groupCreate(group);
-        navigation.navigate('players', { group })
-    }
+    async function handleNew() {
+        try {
+    
+          if(group.trim().length === 0) {
+            return Alert.alert('Novo Grupo', 'Informe o nome da turma.');
+          }
+    
+          await groupCreate(group);
+          navigation.navigate('players', { group });
+        } catch (error) {
+          if(error instanceof AppError) {
+            Alert.alert('Novo Grupo', error.message);
+          } else {
+            Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.');
+            console.log(error);
+          }
+        }
+      }
 
     return(
         <Container>
